5916e658a5b2dc5c1de24f386ccc02ec01077cd1
Volkov Alexander <volkov.525@gmail.com>
2021-06-09 18:21:47 +0300

    add activation page

---
+++ src/components/Activation.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+.all
+	.block
+		h3 1. Создайте файл запроса активации
+		p В консоли настройки Docsvision введите серийный номер вашей лицензии и создайте файл активации.
+	.block
+		h3 2. Загрузите файл запроса активации на сервер Docsvision:
+		.flex
+			v-text-field(label="Выбрать файл")
+			v-btn(flat color="primary" small) Загрузить
+	.block
+		h3 3. Активируйте свою копию Docsvision
+		.flex
+			p Нажимите кнопку "Активировать"
+			v-btn(flat color="primary" small) Активировать
+	.block
+		h3 4. Скачайте сгенерированный файл активации
+		.flex
+			p Сохраните файл активации на свой компьютер
+			v-btn(flat color="primary" small) Скачать
+	.block
+		h3 5. Укажите путь к файлу активации в консоли Docsvision
+		.flex
+			v-text-field(label="Выбрать файл")
+			v-btn(flat color="primary" small) Выбрать
+
+	v-btn(flat color="success" large).middle Активировать
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/css/colors';
+
+.all {
+	width: 900px;
+	margin: 0 auto;
+}
+.block {
+	background: #fff;
+	border: 1px dotted #aaa;
+	border-radius: 5px;
+	padding: 1rem;
+	margin-bottom: 1rem;
+}
+h3 {
+	font-weight: 400;
+	margin-bottom: 1rem;
+}
+p {
+	color: #666;
+	margin-bottom: 0;
+}
+.flex {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	gap: 2rem;
+}
+.middle {
+	display: block;
+	margin: 0 auto;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Folder from '@/components/Folder'
-import Task from '@/components/Task'
-import Doc2 from '@/components/Doc2'
-import History from '@/components/History'
-import Shared from '@/components/Shared'
+// import Folder from '@/components/Folder'
+// import Task from '@/components/Task'
+// import Doc2 from '@/components/Doc2'
+// import History from '@/components/History'
+// import Shared from '@/components/Shared'
+import Activation from '@/components/Activation'
 
 Vue.use(VueRouter)
 
@@ -19,54 +20,10 @@ const routes = [
 		}
 	},
 	{
-		path: '/task',
-		name: 'task',
-		component: Task,
-		meta: {
-			title: 'Задания'
-		}
+		path: '/activation',
+		name: 'activation',
+		component: Activation,
 	},
-	{
-		path: '/docs/1',
-		name: 'doc-shared',
-		component: Shared,
-		meta: {
-			title: 'Документ'
-		}
-	},
-	{
-		path: '/doc2',
-		name: 'doc2',
-		component: Doc2,
-		meta: {
-			title: 'Документы'
-		}
-	},
-	{
-		path: '/folder',
-		name: 'folder',
-		component: Folder,
-		meta: {
-			title: 'Поиск'
-		}
-
-	},
-	{
-		path: '/history',
-		name: 'history',
-		component: History,
-		meta: {
-			title: 'История'
-		}
-	},
-	{
-		path: '/about',
-		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () => import(/* webpackChunkName: "" */ '../views/About.vue')
-	}
 ]
 
 const router = new VueRouter({
